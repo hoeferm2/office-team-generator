286364ae667e4f2b3b4b1fdd5c823ba4987bbd43
Hoefer <ningboninja@gmail.com>
2022-07-20 18:18:00 -0700

    sss

--- 02-Challenge/Develop/lib/Employee.js
+++ 02-Challenge/Develop/lib/Employee.js
@@ -1,7 +1,5 @@
 // TODO: Write code to define and export the Employee class
 
-
-
 class Employee {
     constructor(name, email, id) {
         this.name = name;
@@ -11,6 +9,16 @@ class Employee {
     getName() {
         return this.name;
     }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole() {
+        return 'Employee'
+    }
+
 }
 
 module.exports = Employee
